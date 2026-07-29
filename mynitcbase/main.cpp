9f7e69396817f7871692f0f6a50d4d124384d479
Ellvius <lishinvs6541@gmail.com>
2026-07-29 15:01:10 +0000

    Stage 1:  Understanding the Disk Interface

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -3,11 +3,29 @@
 #include "Disk_Class/Disk.h"
 #include "FrontendInterface/FrontendInterface.h"
 
+#include <cstring>
+#include <iostream>
+
 int main(int argc, char *argv[]) {
   /* Initialize the Run Copy of Disk */
   Disk disk_run;
   // StaticBuffer buffer;
   // OpenRelTable cache;
 
-  return FrontendInterface::handleFrontend(argc, argv);
+  // Test writing into the disk
+  unsigned char buffer[BLOCK_SIZE];
+  Disk::readBlock(buffer, 7000);
+  char message[] = "hello\n";
+  memcpy(buffer + 20, message, 7);
+  Disk::writeBlock(buffer, 7000);
+
+  // Test reading the changes from the disk
+  unsigned char buffer2[BLOCK_SIZE];
+  char message2[7];
+  Disk::readBlock(buffer2, 7000);
+  memcpy(message2, buffer2 + 20, 7);
+  std::cout << message2;
+
+  // return FrontendInterface::handleFrontend(argc, argv);
+  return 0;
 }

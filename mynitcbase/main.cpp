57ceea4e55e9b488c9e5f275d0da0a4d5f41f185
Ellvius <lishinvs6541@gmail.com>
2026-08-05 19:25:05 +0000

    Stage 2: Exercise 1 done

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -12,40 +12,46 @@ int main(int argc, char *argv[]) {
   // StaticBuffer buffer;
   // OpenRelTable cache;
 
-  // create objects for the relation catalog and attribute catalog
+  // Create relation catalog object and load its header into relCatHeader
   RecBuffer relCatBuffer(RELCAT_BLOCK);
-  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
-
-  // Declare Header buffers for relation catalog and attribute catalog
   HeadInfo relCatHeader;
-  HeadInfo attrCatHeader;
-
-  // load the headers of both the blocks into relCatHeader and attrCatHeader.
   relCatBuffer.getHeader(&relCatHeader);
-  attrCatBuffer.getHeader(&attrCatHeader);
-
+  
   // Loop through all the relation catalog records
   for (int i = 0; i < relCatHeader.numEntries; i++) {
-
+    
     // Declare relCatRecord and load the ith relation catalog record into it
     Attribute relCatRecord[RELCAT_NO_ATTRS];
     relCatBuffer.getRecord(relCatRecord, i);
-
+    
     printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+    
+    int currAttrBlock = ATTRCAT_BLOCK;
+    
+    while(currAttrBlock != -1){
 
-    // Loop through all the attribute catalog records
-    for (int j = 0; j < attrCatHeader.numEntries; j++) {
+      // Create current attribute catalog object and load its header into attrCatHeader
+      RecBuffer attrCatBuffer(currAttrBlock);
+      HeadInfo attrCatHeader;
+      attrCatBuffer.getHeader(&attrCatHeader);
 
-      // Declare attrCatRecord and load the jth attribute catalog entry into it
-      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
-      attrCatBuffer.getRecord(attrCatRecord, j);
-
-      // Only print the attribute catalog entry that corresponds to the current relation
-      if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0) {
-        const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
-        printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+      // Loop through all the current attribute catalog records
+      for (int j = 0; j < attrCatHeader.numEntries; j++) {
+  
+        // Declare attrCatRecord and load the jth attribute catalog entry into it
+        Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+        attrCatBuffer.getRecord(attrCatRecord, j);
+  
+        // Only print the attribute catalog entry that corresponds to the current relation
+        if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0) {
+          const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+          printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+        }
       }
+
+      currAttrBlock = attrCatHeader.rblock;
     }
+
     printf("\n");
   }
 
